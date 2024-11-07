845fe4e1d1d3efe8d1d4cf31f418ec95a3db8526
mr-akillraja <mrakillraja@gmail.com>
2024-11-07 13:22:50 +0530

    model evualtion added

---
+++ src/Cauliflower_Classifier/pipeline/stage_04_model_evaluation_mlflow.py
@@ -0,0 +1,33 @@
+from Cauliflower_Classifier.config.configuration import ConfigurationManager
+from Cauliflower_Classifier.components.model_evaluation_mlflow import Evaluation
+from Cauliflower_Classifier import logger
+
+STAGE_NAME = "Evaluation Stage"
+
+
+class EvaluationPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        evaluation_config = config.get_evaluation_config()
+        evaluation = Evaluation(config=evaluation_config)
+        evaluation.evaluation()
+        evaluation.save_score()
+        evaluation.log_into_mlflow()
+
+if __name__ == '__main__':
+
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = EvaluationPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
