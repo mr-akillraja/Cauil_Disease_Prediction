6777a8cbf0b35cefe87a73da08e5154ff9d945a2
mr-akillraja <mrakillraja@gmail.com>
2024-11-10 19:08:52 +0530

    updated common utils

--- src/Cauliflower_Classifier/utils/common.py
+++ src/Cauliflower_Classifier/utils/common.py
@@ -28,12 +28,6 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
 
 @ensure_annotations
 def create_directories(path_to_directories: list, verbose=True):
-    """create list of directories
-
-    Args:
-        path_to_directories (list): list of path of directories
-        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
-    """
     for path in path_to_directories:
         os.makedirs(path, exist_ok=True)
         if verbose:
@@ -42,12 +36,7 @@ def create_directories(path_to_directories: list, verbose=True):
 
 @ensure_annotations
 def save_json(path: Path, data: dict):
-    """save json data
 
-    Args:
-        path (Path): path to json file
-        data (dict): data to be saved in json file
-    """
     with open(path, "w") as f:
         json.dump(data, f, indent=4)
 
@@ -123,3 +112,6 @@ def decodeImage(imgstring, fileName):
 def encodeImageIntoBase64(croppedImagePath):
     with open(croppedImagePath, "rb") as f:
         return base64.b64encode(f.read())
+    
+
+## update the completed
